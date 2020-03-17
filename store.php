0fae315c49859b17b4a92eb78c774b753186e154
Amikawa Rustam <JettoBazooka@gmail.ru>
2020-03-17 13:55:13 +0300

    2/3 fixed

--- store.php
+++ store.php
@@ -1,19 +1,21 @@
 <?php 
 include_once dirname(__FILE__) . '/Comments.php';
 
-// $name='';
-// $email='';
-// $msg='';
+$name='';
+$email='';
+$msg='';
 
 
 	$name = $_POST["name"];
 	$email = $_POST["email"];
 	$msg = $_POST["msg"];
-	Comments::addComment($msg,$email,$name);
+	
 
 	$arrayRes = json_encode([$msg,$email,$name]);
 
+
 echo($arrayRes);
+Comments::addComment($msg,$email,$name);
 
 
 
